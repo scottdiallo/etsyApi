0af94593892102d0bd5c51e2368c1126e0ff9961
Scott Diallo <diallot@me.com>
2016-06-21 13:09:28 -0700

    function to prevent empty search added

--- js/app.js
+++ js/app.js
@@ -11,8 +11,9 @@ $(document).ready(function () {
         //prevent user from sending empty request
         //$('#searchField').empty())
         $('#resultSection').empty();
-        //$('p').text('Searching ... ' + terms).appendTo('#resultSection');
-
+        if (terms == '') {
+            $('.alertmsg').html('you must search for something!');
+        }
         $.getJSON({
             url: etsyUrl,
             dataType: 'JSONP',
@@ -22,22 +23,16 @@ $(document).ready(function () {
                 if (data.count > 0) {
                     $.each(data.results, function (i, results) {
                         $('img').attr('src', results.Images[0].url_75x75).appendTo("#resultSection").wrap("<a href='" + results.url + "'> </a>");
-                        if (i % 4 == 3) {
+
+                        if (i % 6 == 3) {
                             $("</img>").appendTo('#resultSection');
                         }
                     });
-                    //                } else if
-                    //                $('<p>no results!</p>').appendTo('#resultSection');
-                    //
-                    //            } else {
-                    //                $('resultSection').empty();
-                    //                alert(data.error);
-                    //            }
+
                 };
-                //        return false;
                 //console.log(data);
             }
-
         });
+
     });
 });
