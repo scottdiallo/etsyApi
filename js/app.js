d71bb116c035aae18d9731eeddd88aeabb4c5b75
Scott Diallo <diallot@me.com>
2016-06-21 11:48:12 -0700

    api returning data

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     //global variable
     var api_key = "e3sklqilo14vi2q9to4y1jk5";
 
-    // console.log('Hello World!');
+    //console.log('Hello World!');
     $('#submitForm').on('submit', function (event) {
         event.preventDefault();
         var terms = $('#searchField').val();
         //console.log(terms);
-        var etsyUrl = "https://www.openapi.etsy.com/v2/listings/active.js?keywords=" + terms + "&limit=6&includes=Images:1&api_key=" + api_key;
+        var etsyUrl = "https://openapi.etsy.com/v2/listings/active.js?keywords=" + terms + "&limit=6&includes=Images:1&api_key=" + api_key;
         //prevent user from sending empty request
         //$('#searchField').empty())
         $('#resultSection').empty();
@@ -17,11 +17,11 @@ $(document).ready(function () {
             url: etsyUrl,
             dataType: 'JSONP',
             success: function (data) {
-                //console.log(data);
+                console.log(data);
                 $('#resultSection').empty();
                 if (data.count > 0) {
                     $.each(data.results, function (i, item) {
-                        $('img').attr('src', item.Image[0].url_75)
+                        $('img').attr('src', results.Images[0].url_75x75);
                     })
                 }
             }
