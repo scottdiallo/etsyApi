d18435d19935c9282d96f6b4bf44fcf1af63370d
Scott Diallo <diallot@me.com>
2016-06-23 14:00:39 -0700

    minor changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    //global variable
+    //step 1 Define Global Variables
     var api_key = "e3sklqilo14vi2q9to4y1jk5";
 
     //console.log('Hello World!');
@@ -7,10 +7,10 @@ $(document).ready(function () {
         event.preventDefault();
         var terms = $('#searchField').val();
         //console.log(terms);
-        var etsyUrl = "https://openapi.etsy.com/v2/listings/active.js?keywords=" + terms + "&limit=4&includes=Images:1&api_key=" + api_key;
-        //prevent user from sending empty request
-        //$('#searchField').empty())
+        var etsyUrl = "https://openapi.etsy.com/v2/listings/active.js?keywords=" + terms + "&limit=12&includes=Images:1&currency_code&api_key=" + api_key;
         $('#resultSection').empty();
+
+        //prevent empty search
         if (terms == '') {
             $('.alertmsg').html('you must search for something!');
         }
@@ -23,7 +23,6 @@ $(document).ready(function () {
                 if (data.count > 0) {
                     $.each(data.results, function (i, results) {
                         $('img').attr('src', results.Images[0].url_75x75).appendTo("#resultSection").wrap("<a href='" + results.url + "'> </a>");
-
                         if (i % 4 == 3) {
                             $('<br/>').appendTo('#resultSection');
                         }
@@ -33,6 +32,7 @@ $(document).ready(function () {
                 //console.log(data);
             }
         });
+
         //empty input field when form is submitted
         $('#searchField').val('');
 
